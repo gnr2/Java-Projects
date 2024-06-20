1db390aaa6e7bde4259aa42e19110a395537d67c
gnr2 <104433302+gnr2@users.noreply.github.com>
2024-06-20 19:00:29 +0800

    Add files via upload

---
+++ mini-project-1/src/main/java/org/example/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package org.example.calculator;
+
+public class Calculator{
+
+    public static double addNum(double x, double y){
+        return x + y;
+    }
+
+    public static double sbtrctNum(double x, double y){
+        return x - y;
+    }
+
+    public static double divideNum(double x, double y){
+
+        return x / y;
+    }
+
+    public static double mltpyNum(double x, double y){
+        return x * y;
+    }
+
+}
