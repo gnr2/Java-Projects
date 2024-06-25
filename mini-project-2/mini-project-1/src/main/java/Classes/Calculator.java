49c6a440f14b50ea427a880385d12cf9cfe2f936
gnr2 <104433302+gnr2@users.noreply.github.com>
2024-06-26 01:52:39 +0800

    Add mini-project-2

---
+++ mini-project-2/mini-project-1/src/main/java/Classes/Calculator.java
@@ -0,0 +1,22 @@
+package Classes;
+
+public class Calculator{
+
+    public static double addNum(double x, double y){
+        return x + y;
+    }
+
+    public static double sbtrctNum(double x, double y){
+        return x - y;
+    }
+
+    public static double divideNum(double x, double y){
+
+        return x / y;
+    }
+
+    public static double mltpyNum(double x, double y){
+        return x * y;
+    }
+
+}
